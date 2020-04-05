cf8cb162927cbba417f41d47a77bb60fc1fb0b7f
Claudson Oliveira <claudsonweb@gmail.com>
2020-04-05 18:20:28 +0100

    Fix cep with spaces

--- Consulta de endereço pelo CEP.py
+++ Consulta de endereço pelo CEP.py
@@ -8,7 +8,7 @@ print('\nBem-vindo ao sistema de consulta de CEP.')
 
 def cep():
     cep_int = input('Por favor, digite o CEP que você gostaria de procurar na base (ex: 00000000): ')
-    responde = requests.get('https://viacep.com.br/ws/' + cep_int + '/json/')
+    responde = requests.get('https://viacep.com.br/ws/' + cep_int.strip() + '/json/')
     # Verifica se a requisiçao HTTP está disponível.
     if responde.status_code != 200:
         print('Não foi possível acessar o CEP por favor verifique seu número e digite novamente.')
